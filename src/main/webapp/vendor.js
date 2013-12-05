0020ba409c6e5e3ac6793c2e0fab67c084a2f41d
James Strachan <james.strachan@gmail.com>
2013-12-05 10:41:13 +0000

    add better perspective

--- src/main/webapp/vendor.js
+++ src/main/webapp/vendor.js
@@ -2,8 +2,41 @@
 
 // default the perspective
 (function (Perspective) {
-  //Perspective.defaultPerspective = "wiki";
+  var metadata = Perspective.metadata || {};
+  Perspective.defaultPerspective = "CamelWiki";
   Perspective.defaultPageLocation = "#/wiki/view";
+
+  metadata["CamelWiki"] = {
+    label: "Camel Wiki",
+    isValid: function (workspace) { return true; },
+    lastPage: "#/wiki/view",
+    topLevelTabs: {
+      includes: [
+        {
+          href: "#/wiki"
+        },
+        {
+          id: "activemq"
+        },
+        {
+          id: "camel"
+        },
+        {
+          id: "jmx"
+        },
+        {
+          href: "#/dashboard"
+        },
+        {
+          href: "#/health"
+        },
+        {
+          href: "#/logs"
+        }
+      ]
+    }
+  };
+
 })(Perspective || {});
 
 
